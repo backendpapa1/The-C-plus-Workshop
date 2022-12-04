b945cdd9675b7ef055c123cf459f62cdaf2ebbfe
backendpapa1 <akejupaul10@gmail.com>
2022-12-04 02:48:57 +0100

    git push

--- main.cpp
+++ main.cpp
@@ -4,25 +4,23 @@
 
 int main() {
 
-    std::string input;
-    int number;
+    int myInt = 1;
+    bool myBool = false;
+    char myChar = 'a';
 
-    std::cout << "Please enter a number: ";
-    getline(std::cin,input);
 
-    try {
-        number = std::stoi(input);
-    }
-    catch (const std::invalid_argument & e) {
-        std::cout << e.what() << "\n";
-    }
+    std::cout << "The size of a int is " << sizeof(myInt) << std::endl;
+    std::cout << "The size of a bool is " << sizeof(myBool) << std::endl;
+    std::cout << "The size of a char is " << sizeof(myChar) << std::endl;
 
-    if(number < 10){
-        std::cout << "The number you entered was less than 10! \n";
-    }else if(number > 10) {
-        std:: cout << "The number you entered was greater than 10! \n";
-    }
-    number < 10 ? std::cout << "The number you entered was less than 10! \n" : std:: cout << "The number you entered was greater than 10! \n";
 
+    int customerAges[5];
+
+    std::cout << &customerAges[0] << std::endl;
+    std::cout << &customerAges[1] << std::endl;
+    std::cout << &customerAges[2] << std::endl;
+    std::cout << &customerAges[3] << std::endl;
+    std::cout << &customerAges[4] << std::endl;
+    std::cout << sizeof(customerAges) / sizeof (customerAges[0]) << std::endl;
     return 0;
 }
